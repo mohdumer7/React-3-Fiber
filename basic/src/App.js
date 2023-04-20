9032a01d1619fcffd29f409ad058bcdc5e344182
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-20 16:01:52 +0530

    testing meshes and rotation

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,5 +1,30 @@
+import { useRef } from "react";
+import "./style.css";
+import { useFrame } from "@react-three/fiber";
+
 function App() {
-  return <div>Hello..!!</div>;
+  const cube = useRef();
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta;
+  });
+  return (
+    <>
+      <group ref={cube}>
+        <mesh>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+        <mesh position-x={2}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+      <mesh scale={10} position-y={-1} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
 }
 
 export default App;
--- basic/src/index.js
+++ basic/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import { Canvas, useFrame } from "@react-three/fiber";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <App />
+    </Canvas>
   </React.StrictMode>
 );
