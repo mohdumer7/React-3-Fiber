b5b3440b4019a7460314186472c9f0d754bea14f
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-20 21:35:13 +0530

    Added custom OrbitControls

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,34 +1,28 @@
 import { useRef } from "react";
 import "./style.css";
-import { useFrame, extend, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-extend({ OrbitControls });
+import { TransformControls, OrbitControls } from "@react-three/drei";
 
 function App() {
   const cube = useRef();
-  const { camera, gl } = useThree();
-
-  useFrame((state, delta) => {
-    cube.current.rotation.y += delta;
-  });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
       <group>
-        <mesh ref={cube}>
+        <mesh>
           <boxGeometry />
           <meshStandardMaterial color="mediumpurple" />
         </mesh>
-        <mesh position-x={2}>
+
+        <mesh ref={cube} position-x={2}>
           <boxGeometry />
           <meshBasicMaterial color="mediumpurple" />
         </mesh>
+        <TransformControls object={cube} />
       </group>
       <mesh scale={10} position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry />
--- basic/src/index.js
+++ basic/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Canvas, useFrame } from "@react-three/fiber";
 import App from "./App";
+import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Canvas>
+    <Canvas camera={{}}>
       <App />
     </Canvas>
   </React.StrictMode>
