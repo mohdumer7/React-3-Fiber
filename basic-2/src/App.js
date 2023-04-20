956a232f1583a42925f186ef3d2917a577e764b8
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-20 23:52:48 +0530

    Added Html overlays

---
+++ basic-2/src/App.js
@@ -0,0 +1,58 @@
+import { useRef } from "react";
+import "./style.css";
+import {
+  PivotControls,
+  TransformControls,
+  OrbitControls,
+  Html,
+} from "@react-three/drei";
+
+function App() {
+  const cube = useRef();
+  const cube1 = useRef();
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <group>
+        <PivotControls anchor={[0, 0, 0]} depthTest={false}>
+          <mesh position-x={2} ref={cube1}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+            <Html
+              position={[1, 1, 0]}
+              wrapperClass="label"
+              center
+              distanceFactor={6}
+              occlude={[cube, cube1]}
+            >
+              cube
+            </Html>
+          </mesh>
+        </PivotControls>
+
+        <mesh ref={cube} position-x={2}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={cube} />
+      </group>
+      <mesh scale={10} position-y={-1} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      <Html>
+        umer
+        <div
+          style={{ height: "50px", width: "100px", backgroundColor: "white" }}
+        ></div>
+      </Html>
+    </>
+  );
+}
+
+export default App;
