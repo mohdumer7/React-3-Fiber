8c37726b0b33a462968a6991df947283e8b13723
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-21 23:33:58 +0530

    Added the Model

--- Portal/src/App.js
+++ Portal/src/App.js
@@ -1,6 +1,6 @@
 import "./style.css";
-import { useTexture, OrbitControls, useGLTF } from "@react-three/drei";
-
+import { useTexture, OrbitControls, useGLTF, Center } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 function App() {
   const { nodes } = useGLTF("./portal.glb");
   console.log(nodes);
@@ -11,7 +11,7 @@ function App() {
     <>
       <color args={["#201919"]} attach="background" />
       <OrbitControls makeDefault />
-
+      <Perf position="top-left" />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
@@ -19,10 +19,31 @@ function App() {
         <boxGeometry />
         <meshNormalMaterial color="greenyellow" />
       </mesh> */}
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedtexture} />
+        </mesh>
 
-      <mesh geometry={nodes.baked.geometry}>
-        <meshBasicMaterial map={bakedtexture} />
-      </mesh>
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        ></mesh>
+      </Center>
     </>
   );
 }
