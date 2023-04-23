88f695a25f3bed70726fa287e05db393a2cc3503
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-04-23 16:46:45 +0530

    Added SSR

--- PostProcessing/src/App.js
+++ PostProcessing/src/App.js
@@ -20,6 +20,7 @@ import {
   Glitch,
   Noise,
   Vignette,
+  SSR,
   DepthOfField,
 } from "@react-three/postprocessing";
 
@@ -62,9 +63,40 @@ function App() {
     choice: { options: ["a", "b", "c"] },
   });
 
+  const ssr_controls = useControls({
+    temporalResolve: true,
+    STRETCH_MISSED_RAYS: true,
+    USE_MRT: true,
+    USE_NORMALMAP: true,
+    USE_ROUGHNESSMAP: true,
+    ENABLE_JITTERING: true,
+    ENABLE_BLUR: true,
+    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+    maxSamples: { value: 0, min: 0, max: 1 },
+    resolutionScale: { value: 1, min: 0, max: 1 },
+    blurMix: { value: 0.5, min: 0, max: 1 },
+    blurKernelSize: { value: 8, min: 0, max: 8 },
+    blurSharpness: { value: 0.5, min: 0, max: 1 },
+    rayStep: { value: 0.3, min: 0, max: 1 },
+    intensity: { value: 1, min: 0, max: 5 },
+    maxRoughness: { value: 0.1, min: 0, max: 1 },
+    jitter: { value: 0.7, min: 0, max: 5 },
+    jitterSpread: { value: 0.45, min: 0, max: 1 },
+    jitterRough: { value: 0.1, min: 0, max: 1 },
+    roughnessFadeOut: { value: 1, min: 0, max: 1 },
+    rayFadeOut: { value: 0, min: 0, max: 1 },
+    MAX_STEPS: { value: 20, min: 0, max: 20 },
+    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+    maxDepthDifference: { value: 3, min: 0, max: 10 },
+    maxDepth: { value: 1, min: 0, max: 1 },
+    thickness: { value: 10, min: 0, max: 10 },
+    ior: { value: 1.45, min: 0, max: 2 },
+  });
+
   return (
     <>
-      <color args={["#000"]} attach="background" />
+      <color args={["#2a2a2a"]} attach="background" />
       <Perf position="top-left" />
 
       <EffectComposer>
@@ -75,13 +107,18 @@ function App() {
           mode={GlitchMode.CONSTANT_MILD}
         />
 
-        <Glitch delay={[0.5, 1]} duration={[0.1, 0.3]} strength={[0.2, 0.4]} />
+        <Glitch delay={[8, 10]} duration={[0.1, 0.3]} strength={[0.2, 0.4]} />
 
         <Noise blendFunction={BlendFunction.SOFT_LIGHT} />
 
         <Bloom mipmapBlur />
 
-        <DepthOfField />
+        <DepthOfField
+          focusDistance={0.025}
+          focusLength={0.025}
+          bokehScale={10}
+        />
+        {/* <SSR {...ssr_controls} /> */}
       </EffectComposer>
 
       <OrbitControls makeDefault />
@@ -159,6 +196,8 @@ function App() {
           resolution={512}
           blur={(1000, 1000)}
           mirror={0.5}
+          roughness={0}
+          metalness={0}
         />
       </mesh>
 
